a5239fe14993ea0dc6ceae8adea4796fd694f6cb
Smit <smitshah95@hotmail.com>
2020-05-10 23:09:58 +0800

    Updates

---
+++ precache-manifest.97b35ec24f39f6bf42a6609d618b77b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae032897b04ae7e821dcb34ef73b591d",
+    "url": "/jb-bio-medical/index.html"
+  },
+  {
+    "revision": "3a919b65498d92a397c8",
+    "url": "/jb-bio-medical/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a34d3c57513d05714623",
+    "url": "/jb-bio-medical/static/js/2.abfd53ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jb-bio-medical/static/js/2.abfd53ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a919b65498d92a397c8",
+    "url": "/jb-bio-medical/static/js/main.ab3fd195.chunk.js"
+  },
+  {
+    "revision": "f94934c57abbadb2c6dc",
+    "url": "/jb-bio-medical/static/js/runtime-main.2e44508f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jb-bio-medical/static/media/logo.5d5d9eef.svg"
+  }
+]);
