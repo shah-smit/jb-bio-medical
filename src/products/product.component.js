b1da7d5678d7a1dc88c11b45bae9bd128863727d
Smit <smitshah95@hotmail.com>
2020-05-11 20:22:25 +0800

    added pdf viewer into it

--- src/products/product.component.js
+++ src/products/product.component.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Product } from './product';
 import { ProductService } from './products.service'
 import { Col, Row, Container, Image } from 'react-bootstrap';
+import { Document, Page } from 'react-pdf';
 
 class ProductComponent extends React.Component {
+
     products: Product[];
 
     constructor(props) {
         super(props);
         console.log("props")
-        
+
         this.productId = props.match.params.id;
         console.log(this.productId)
         this.product = ProductService.getProductById(this.productId)
@@ -21,10 +23,11 @@ class ProductComponent extends React.Component {
     render() {
         return (
             <div>
-                <Image src={process.env.PUBLIC_URL + this.product.ImageUrl} height="100" width="150" thumbnail /><br/>
-                <span>Id: {this.productId}</span><br/>
-                <span>Name: {this.product.Name}</span><br/>
-                <span>Description: {this.product.Description}</span><br/>
+                <Image src={process.env.PUBLIC_URL + this.product.ImageUrl} height="100" width="150" thumbnail /><br />
+                <span>Id: {this.productId}</span><br />
+                <span>Name: {this.product.Name}</span><br />
+                <span>Description: {this.product.Description}</span><br />
+                <a href={process.env.PUBLIC_URL + this.product.PdfUrl}>test</a>
             </div>);
     }
 }
