ede211dc4e7b0f08e8b9c0cc7ab95224e0b70561
Smit <smitshah95@hotmail.com>
2020-05-20 00:34:08 +0800

    added search in navbar

--- src/helper/Nav.js
+++ src/helper/Nav.js
@@ -1,21 +1,46 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom'
 
-function CustomNavbar(props) {
-  console.log(props)
-  return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">JB BIO Med</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#products">Catalogue</Nav.Link>
-          <Nav.Link href="#contact">Contact Us</Nav.Link>
-          <Nav.Link href="#about">About Us</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
-  );
+class CustomNavbar extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.onChangeSearchField = this.onChangeSearchField.bind(this)
+    this.onSearchClicked = this.onSearchClicked.bind(this)
+
+    this.state = {
+      searchField: ""
+    }
+  }
+
+  onChangeSearchField(e) {
+    this.setState({ searchField: e.target.value });
+  }
+
+  onSearchClicked(e) {
+    this.props.history.push('/products?search='+this.state.searchField)
+  }
+
+  render() {
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">JB BIO Med</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#products">Catalogue</Nav.Link>
+            <Nav.Link href="#contact">Contact Us</Nav.Link>
+            <Nav.Link href="#about">About Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        <Form inline>
+          <FormControl type="text" placeholder="Search" value={this.state.searchField} onChange={this.onChangeSearchField} className="mr-sm-2" />
+          <Button variant="outline-success" onClick={this.onSearchClicked}>Search</Button>
+        </Form>
+      </Navbar>
+    );
+  }
 }
 
-export default CustomNavbar;
+export default withRouter(CustomNavbar);
