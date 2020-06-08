c3e0ea527848539d54635f997094e3a473f2f3a0
Smit <smitshah95@hotmail.com>
2020-06-08 16:40:27 +0800

    added banner

--- src/home/home.component.js
+++ src/home/home.component.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import { Link } from 'react-router-dom'
+import { Image,Carousel } from 'react-bootstrap'
 
 import CustomNavbar from '../helper/Nav'
 
@@ -9,17 +10,31 @@ class HomeComponent extends React.Component {
     render() {
         return (
             <>
-            <CustomNavbar currentPage="products"/>
-            <div className="App">
-                <header className="App-header">
-                    <img src={process.env.PUBLIC_URL + "/images/logo.png"} className="App-logo" alt="logo" />
+                <CustomNavbar currentPage="products" />
+                <div className="App">
+                    {/* <header className="App-header"> */}
+                        {/* <img src={process.env.PUBLIC_URL + "/images/logo.png"} className="App-logo" alt="logo" />
                     <p>
                     Welcome to JB Bio Medical Equipments Showcase!
                     </p>
                     <Link to="/products">Catalogue</Link>
-                    <Link to="/contact">Contact Us</Link>
-                </header>
-            </div>
+                    <Link to="/contact">Contact Us</Link> */}
+                        {/* <Image src={process.env.PUBLIC_URL + "/images/banner.png"} fluid />
+                    </header> */}
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src={process.env.PUBLIC_URL + "/images/banner.png"}
+                                alt="First slide"
+                            />
+                            {/* <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption> */}
+                        </Carousel.Item>
+                    </Carousel>
+                </div>
             </>
         );
     }
